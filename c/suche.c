7fb194251f906a432003ca490a76fa30de285eaa
hung2388 <vusinhhung238@gmail.com>
2026-03-06 14:37:31 +0700

    sudoku

---
+++ c/suche.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int n;
+int main()
+{
+    scanf("%d",&n);
+    while(n--)
+    {
+        int col[9][10]={0};
+        int row[9][10]={0};
+        int square[3][3][10]={0};
+        int A[9][9]={0};
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                scanf("%d",A[i][j]);
+            }
+        }
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                
+            }
+        }
+    }
+}
