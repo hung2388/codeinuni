1748cff00141356c57f8fef439fe6658ad007b0b
hung2388 <vusinhhung238@gmail.com>
2026-03-07 07:36:44 +0700

    sudoku

--- c/suche.c
+++ c/suche.c
@@ -13,15 +13,37 @@ int main()
         {
             for(int j=0;j<9;j++)
             {
-                scanf("%d",A[i][j]);
+                scanf("%d",&A[i][j]);
             }
         }
+        int check =1;
         for(int i=0;i<9;i++)
         {
             for(int j=0;j<9;j++)
             {
-                
+                int c=A[i][j];
+                if(c>9||c<1){
+                    check=0;
+                    break;
+                }
+                if(col[j][c]==0) col[j][c]=1;
+                else {
+                    check=0;
+                    break;
+                }
+                if(row[i][c]==0) row[i][c]=1;
+                else {
+                    check=0;
+                    break;
+                }
+                if(square[i/3][j/3][c]==0) square[i/3][j/3][c]=1;
+                else {
+                    check=0;
+                    break;
+                }
             }
+            if(check==0) break;
         }
+        printf("%d\n",check);
     }
 }
