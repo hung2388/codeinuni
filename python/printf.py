1172f672fe67fad128aef7e27b85a00e33f27291
hung2388 <vusinhhung238@gmail.com>
2026-04-04 17:43:36 +0700

    end = '' la ko xuong dòng

--- python/printf.py
+++ python/printf.py
@@ -6,3 +6,5 @@ print(f"hello {number:.2f}")
 print(f"{number:010}") #in trong khung rộng 10 kí tự, và thêm số 0 vào đầu
 print(f"{number:,}")#grand format
 print(f"{number:+,.2f}")
+for i in range(11):
+    print(f"{i}",end="") #end="" là ko xuống dòng
