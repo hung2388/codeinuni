e9d51e35dae3068e4d76dd69fde7d5bb0b76c0fe
hung2388 <vusinhhung238@gmail.com>
2026-04-04 17:32:58 +0700

    real

---
+++ python/printf.py
@@ -0,0 +1,8 @@
+#num=input("num ")
+#print(num[0:8:2])
+#print(num[-1])
+number=998124.2345
+print(f"hello {number:.2f}")
+print(f"{number:010}") #in trong khung rộng 10 kí tự, và thêm số 0 vào đầu
+print(f"{number:,}")#grand format
+print(f"{number:+,.2f}")
