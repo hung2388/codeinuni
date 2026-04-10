340a1fcbfcb11bb09c69e1a9798d5b7ec4ba1909
hung2388 <vusinhhung238@gmail.com>
2026-04-10 09:12:14 +0700

    hntown

---
+++ c/GCD.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int gcd(int a,int b)
+{
+    while(b!=0)
+    {
+        int r=a%b;
+        a=b;
+        b=r;
+    }
+    return a;
+}
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a<b)
+    printf("%d",gcd(b,a));
+    else
+    printf("%d",gcd(a,b));
+}
---
+++ c/hntown.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+void Try(int n, int a, int b, int c)
+{
+    if(n==1)
+    {
+        printf("%d %d\n",a,c);
+        return;
+    }
+    Try(n-1,a,c,b);
+    printf("%d %d\n",a,c);
+    Try(n-1,b,a,c);
+}
+int main()
+{
+    int n,a,b,c;
+    scanf("%d %d %d %d",&n,&a,&b,&c);
+    printf("%d\n",(int)pow(2,n)-1);
+    Try(n,a,b,c);
+}
