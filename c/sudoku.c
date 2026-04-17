24698a17e28715f519009335efab005fdbb08658
hung2388 <vusinhhung238@gmail.com>
2026-04-17 09:23:58 +0700

    remember return

--- c/sudoku.c
+++ c/sudoku.c
@@ -3,12 +3,72 @@ int A[9][9];
 int R[9][10];
 int C[9][10];
 int Blck[3][3][10];
+int cnt=0;
+int check(int i, int j, int k)
+{
+    if(R[i][k]==1||C[j][k]==1||Blck[i/3][j/3][k]==1)
+    {
+        return 0;
+    }
+    return 1;
+}
+void try(int i, int j)
+{
+    if(i==9)
+    {
+        cnt++;
+        return;
+    }
+    else if(j==9)
+    {
+        try(i+1,0);
+        return;
+    }
+    if(A[i][j]!=0)
+    {
+            try(i,j+1);
+            return; 
+    }
+    for(int k=1;k<=9;k++)
+    {
+        
+        if(check(i,j,k))
+        {
+            A[i][j]=k;
+            R[i][k]=1;
+            C[j][k]=1;
+            Blck[i/3][j/3][k]=1;
+
+                    try(i,j+1);
+            
+            A[i][j]=0;
+            R[i][k]=0;
+            C[j][k]=0;
+            Blck[i/3][j/3][k]=0;
+        }
+    }
+}
 
 int main()
 {
-            R[9][10]={0};
-            C[9][10]={0};
-    Blck[3][3][10]={0};
+    for(int i=0;i<9;i++)
+    {
+        for(int j=0;j<=9;j++)
+        {
+            R[i][j]=0;
+            C[i][j]=0;
+        }
+    }
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            for(int k=0;k<10;k++)
+            {
+                Blck[i][j][k]=0;
+            }
+        }
+    }
     for(int i=0;i<9;i++)
     {
         for(int j=0;j<9;j++)
@@ -16,10 +76,12 @@ int main()
             scanf("%d",&A[i][j]);
             if(A[i][j]!=0)
             {
-                R[i][A[i][j]]==1;
-                C[j][A[i][j]]==1;
-                
+                R[i][A[i][j]]=1;
+                C[j][A[i][j]]=1;
+                Blck[i/3][j/3][A[i][j]]=1;
             }
         }
     }
+    try(0,0);
+    printf("%d",cnt);
 }
